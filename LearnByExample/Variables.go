84fee958444c248ce88c7260a95682541503e1e4
Hazem AbdEl-Aziz <hazemsoliman0813@gmail.com>
2024-08-15 13:28:48 +0300

    Add variables.go
    Variables

---
+++ LearnByExample/Variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func variables() {
+
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 5, 5
+	println(b, c)
+
+}
--- LearnByExample/hello.go
+++ LearnByExample/hello.go
@@ -3,13 +3,7 @@ package main
 import "fmt"
 
 func main() {
-
-	var a = "initial"
-	fmt.Println(a)
-
-	var b, c int = 5, 5
-	println(b, c)
-
 	fmt.Println("hello world")
 
+	variables()
 }
